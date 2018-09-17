c52063875e51e2fc2cb39449fa82ab530499661b
Minh Nguyen <minhnguyen93@outlook.com>
2018-09-17 22:04:14 +0300

    Week 1 - Task3

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 
 
-#### Assignment
+#### Assignment 1
 Implementing a node to be both a publisher and an subscriber.
 1. _Task 1_:
 	An ultrasound sensor is installed in a system. It is used for measuring the height of the boxes running on a conveyor belt. When there is no box, the sensor publishes the maximum range and when detecting one, it reports the distance to the top surface of the box. The  ***week1_assignment1.py*** is modified to achieve:
@@ -17,7 +17,7 @@ Implementing a node to be both a publisher and an subscriber.
   2. Compute the height of the box from the sensor reading.
   3. Filter out the false positives from the sensor due to sensor noise.
 
-	The result is located in the directory *src/hrwros/hwros_week1/scripts/week1_task1.png*
+	The result is located in the directory *src/hrwros/hwros_week1/scripts/week1_assignment1_task1.png*
 2. _Task 2_: Create a new message type called BoxHeightInformation.msg containing the placeholder "box_height" which is a floating point number.
   1. Create a new message type ***BoxHeightInformation.msg*** in the same folder where ***SensorInformation.msg*** file is located.
 
@@ -28,7 +28,7 @@ Implementing a node to be both a publisher and an subscriber.
 	rosmsg show hrwros_msgs/BoxHeightInformation
 	```
 
-	The screenshot of the result is located in the directory *src/hrwros/hwros_week1/scripts/week1_task2.png*
+	The screenshot of the result is located in the directory *src/hrwros/hwros_week1/scripts/week1_assignment1_task2.png*
 3. _Task 3_: Only publish a new topic "/box_height_info" when a valid box is detected.
   1. Create an object of the new BoxHeightInformation message type when the detected box height is valid.
 	2. Create a publisher for the new massage type.
@@ -53,7 +53,7 @@ Implementing a node to be both a publisher and an subscriber.
 	```
 	After 5 messages, terminate the execution.
 
-	The screenshot of the result is located in the directory *src/hrwros/hwros_week1/scripts/week1_task3.png*
+	The screenshot of the result is located in the directory *src/hrwros/hwros_week1/scripts/week1_assignment1_task3.png*
 
 
 
--- src/hrwros/hrwros_week1/scripts/week1_assignment1.py
+++ src/hrwros/hrwros_week1/scripts/week1_assignment1.py
@@ -8,34 +8,35 @@
 
 # All necessary python imports go here.
 import rospy
-from hrwros_msgs.msg import SensorInformation
+from hrwros_msgs.msg import SensorInformation, BoxHeightInformation
+from hrwros_utilities.sim_sensor_data import distSensorData as getSensorData
 
 def sensor_info_callback(data, bhi_pub):
     # Compute the height of the box.
-    height_box = data.sensor_data.max_range - data.sensor_data_range
+    height_box = data.sensor_data.max_range - data.sensor_data.range
     # Boxes that are detected to be shorter than 10cm are due to sensor noise.
     # Do not publish information about them.
-    if height_box < 10:
+    if height_box < 0.10:
         pass
     else:
         # Declare a message object for publishing the box height information.
-        box_height_info = SensorInformation()
+        box_height_info = BoxHeightInformation()
         # Update height of box.
-        box_height_info.sensor_data = height_box
+        box_height_info.box_height = height_box
         # Publish box height using the publisher argument passed to the callback function.
-        bhi_pub.publish(box_heigh_info)
+        bhi_pub.publish(box_height_info)
 
 if __name__ == '__main__':
     # Initialize the ROS node here.
     rospy.init_node('compute_box_height', anonymous = False)
 
     # Wait for the topic that publishes sensor information to become available.
-    rospy.loginfo('Waiting for topic %s to be published...', sensor_info)
+    rospy.loginfo('Waiting for topic %s to be published...', 'sensor_info')
     rospy.wait_for_message('sensor_info', SensorInformation)
-    rospy.loginfo('%s topic is now available!', sensor_info)
+    rospy.loginfo('%s topic is now available!', 'sensor_info')
 
     # Create the publisher and subscriber here.
-    bhi_publisher = rospy.Publisher('sensor_info', SensorInformation, queue_size=10)
+    bhi_publisher = rospy.Publisher('box_height_info', BoxHeightInformation, queue_size=10)
     # Note here that an ADDITIONAL ARGUMENT (bhi_publisher) is passed to the subscriber. This is a way to pass
     # ONE additional argument to the subscriber callback. If you want to pass multiple arguments,
     # you can use a python dictionary. And if you don't want to use multiple arguments to the
